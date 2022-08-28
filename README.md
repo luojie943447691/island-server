d7648dab30644bc6be24b4c72f9f0d64d13fe650
USER <943447691@qq.com>
2022-08-28 18:13:02 +0800

    feat: 批量导入配置。

---
+++ README.md
@@ -0,0 +1,2 @@
+# package.json 文件说明
+- require-directory 读取某个文件夹下的所有 文件
--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const Koa = require('koa')
-const book = require("./api/v1/book")
-const classic = require("./api/v1//classic")
+var requireDirectory = require('require-directory');
+const Router = require('koa-router')
 
 const app = new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+// module 固定写法
+// "./api/v1" 指定拥有 router 的路径
+const modeuls = requireDirectory(module, "./app/api/v1", {
+    visit: (obj) => {
+        // 判断当前对象是否是一个Router，这种判断方式很简单，只适用于导出时没有使用大括号的方式，如果使用了大括号，这里的判断逻辑相对就会复杂一些
+        if (obj instanceof Router) {
+            app.use(obj.routes());
+        }
+    }
+})
+console.log('modeuls',modeuls);
 
 app.listen(3000, () => {
-    console.log("已开始监听3000端口111！");
+    console.log("已开始监听3000端口！");
 })
